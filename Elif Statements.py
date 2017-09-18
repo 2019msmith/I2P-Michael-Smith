075120b2764b7cd51bef8329bf352f887c043bf2
2019msmith <31870742+2019msmith@users.noreply.github.com>
2017-09-18 19:22:14 +0800

    Add files via upload

--- Elif Statements.py
+++ Elif Statements.py
@@ -6,5 +6,3 @@ elif Value1 < Value2:
     print("Number 2 is greater than Number 1")
 else:
     print("Your two numbers are equal!")
-input("\n \n Please press enter to exit the program")
-
---
+++ For... In... Loops.py
@@ -0,0 +1,5 @@
+name = "Michael"
+i=0
+for letter in name:
+    print (name[i])
+    i = i+1
